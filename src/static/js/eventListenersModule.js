ba3731cd1e2876fbbe288f580ef0aa102f5c2924
Mayank Nader <mayank.nader11@gmail.com>
2020-10-22 20:12:22 +0530

    create and attach login and logout listeners

--- src/static/js/eventListenersModule.js
+++ src/static/js/eventListenersModule.js
@@ -8,7 +8,7 @@ async function postData(url = "", data = {}) {
     },
     body: JSON.stringify(data),
   });
-  return response.json();
+  return response;
 }
 
 async function handleRegister(event) {
@@ -32,7 +32,57 @@ async function handleRegister(event) {
   }
 }
 
+async function handleLogin(event) {
+  console.log("clicked");
+  console.log(event.target);
+  if (event.target.classList.contains("login-button")) {
+    const usernameInput = document.getElementById("login-username-input");
+    const passwordInput = document.getElementById("login-password-input");
+    const dropDown = document.getElementById("login-dropdown");
+    if (!usernameInput.value || !passwordInput.value || !dropDown.value) {
+      alert("Error: Empty username or password or role");
+      throw Error("empty username or password or role");
+    }
+    const data = {
+      username: usernameInput.value,
+      password: passwordInput.value,
+      role: dropDown.value,
+    };
+
+    const response = await postData("http://localhost:5000/login/", data);
+    const receivedData = await response.json();
+    if (response.status === 200) {
+      const { username, password, role } = receivedData.results;
+      appObject.setItem("username", username);
+      appObject.setItem("password", password);
+      appObject.setItem("role", role);
+
+      window.location = "/dashboard";
+    } else {
+      alert(`${receivedData.message}`);
+    }
+
+    console.log(appObject);
+  }
+}
+
+async function handleLogout(event) {
+  console.log("clicked");
+  console.log(event.target);
+  if (event.target.id == "logout-link") {
+    if (appObject.getItem("username") && appObject.getItem("password")) {
+      appObject.clear();
+      window.location = "/";
+    } else {
+      alert("invalid action");
+    }
+
+    console.log(appObject);
+  }
+}
 
 export default function addListeners() {
   elements.app.addEventListener("click", handleRegister);
+  elements.app.addEventListener("click", handleLogin);
+  elements.app.addEventListener("click", handleLogout);
 }
--- src/static/js/views/LoginView.js
+++ src/static/js/views/LoginView.js
@@ -1,4 +1,5 @@
-import AbstractView from "./AbstractView.js";
+import AbstractView from "./AbstractView";
+import { appObject } from "../base";
 
 export default class extends AbstractView {
   constructor(params) {
@@ -7,24 +8,21 @@ export default class extends AbstractView {
   }
 
   async getHtml() {
-    function handleLogin(event) {
-      const usernameInput = document.getElementById("username-input");
-      const passwordInput = document.getElementById("password-input");
-      // client side validation
+    if (appObject.getItem("username") && appObject.getItem("password")) {
+      console.log("app Object verified");
+      window.location = "/dashboard";
     }
-
-    const button = document.createElement("button");
-    button.classList.add("login-button");
-    button.addEventListener("click", handleLogin);
-    console.log("trying navigate to");
-    // navigateTo("/register");
 
     return `
             <h1>Login</h1>
             <label for="username"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="username" id="username-input" required>  
+            <input type="text" placeholder="Enter Username" name="username" id="login-username-input">  
             <label for="password"><b>Username</b></label>
-            <input type="text" placeholder="Enter Password" name="password" id="password-input" required>  
+            <input type="text" placeholder="Enter Password" name="password" id="login-password-input">  
+            <select id="login-dropdown" name="role">
+              <option value="student">Student</option>
+              <option value="teacher">Teacher</option>
+            </select>
             <button class="login-button">Login</button>
         `;
   }
